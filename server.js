3bd95a733f539941be05c6bb9a761259d408f9f0
siddhesh upkare <supkare@chirpn.com>
2021-10-07 21:45:54 +0530

    third commit

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(express.static(__dirname + '/dist/movies'));
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/movies/index.html'));});
+'/dist/myapp/index.html'));});
 
 app.listen(process.env.PORT || 8080);
